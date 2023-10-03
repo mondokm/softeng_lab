e2791c84110a268d6ff4a63ab6363d0d6724eefb
Mondok Milán <milanmondok1998@gmail.com>
2023-10-03 09:57:41 +0200

    Update GT4500.java

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -17,7 +17,7 @@ public class GT4500 implements SpaceShip {
 
   public boolean fireLaser(FiringMode firingMode) {
     // TODO not implemented yet
-    System.out.println("Not implemented yet.")
+    System.out.println("Not implemented yet.");
     return false;
   }
 
